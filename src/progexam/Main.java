117387ede8dfd215db7448c3a151115affc677b1
hatomachikadai <e195728@ie.u-ryukyu.ac.jp>
2019-12-09 11:43:40 +0900

    エラー文

--- src/progexam/Main.java
+++ src/progexam/Main.java
@@ -3,14 +3,14 @@ package progexam;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         String str = null;
         System.out.println(str.length());
 
-}
-
-
     }
+
+
+}
 
 
 
