cd6b1b3686d2dd9d2cd57509c7afaf0d98e2c289
hatomachikadai <e195728@ie.u-ryukyu.ac.jp>
2019-12-09 20:18:17 +0900

    エラー文２

--- src/progexam/Main.java
+++ src/progexam/Main.java
@@ -3,19 +3,20 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        try {
-            String str = null;
-            System.out.println(str.length());
+        //try
+        //String str = null;
+        //System.out.println(str.length());
 
 
-        } catch (NullPointerException e) {
-            System.out.println("nullが入力されています");
-            System.out.println(e.getMessage());
+        //} catch (NullPointerException e) {
+        //System.out.println("nullが入力されています");
+        //System.out.println(e.getMessage());
+        //}
+            String str = "百二十三";
+            int value = Integer.parseInt(str);
         }
 
     }
-}
-
 
 
 
